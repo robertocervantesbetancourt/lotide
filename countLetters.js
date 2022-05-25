86c369c4f3fb9153b172c38ce99e128d1ae8292e
Roberto Cervantes Betancourt <roberto.cervantes.bet@gmail.com>
2022-05-25 15:20:52 +0000

    create countLetters algorithm

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual}  ===  ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  const result = {};
+  for (const letter of sentence) {
+    if (result[letter]){
+      result[letter] += 1;
+    } else {
+      result[letter] = 1;
+    }
+  }
+  console.log(result);
+  return result;
+};
+
+const result1 = countLetters('lighthouse in the house');
+
+assertEqual(result1['h'],4);
+assertEqual(result1['e'],3);
+assertEqual(result1['z'],undefined);
