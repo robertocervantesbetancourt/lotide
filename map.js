1f8f25811a431a5e5e6c28dce31c577e6b9b5035
Roberto Cervantes Betancourt <roberto.cervantes.bet@gmail.com>
2022-05-27 21:31:46 +0000

    implement map algorithm for lotie

---
+++ map.js
@@ -0,0 +1,35 @@
+const eqArrays = function(arr1, arr2) {
+  for (let x = 0; x < arr1.length; x++) {
+    if (arr1[x] !== arr2[x]) {
+      return false;
+    }
+  } 
+  return true;
+};
+
+const assertArraysEqual = function(value) {
+  if (value === false) {
+    console.log('Arrays are NOT equal');
+    return
+  } 
+  console.log('Both arrays are equal');
+  return
+}
+
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+
+const map = function(array, callback){
+  const results = [];
+  for (let item of array){
+    results.push(callback(item));
+  }
+
+  return results;
+}
+
+assertArraysEqual(eqArrays(map(words, word => word),words));
+
+const result = map(words, word => word[0]);
+console.log(result);
