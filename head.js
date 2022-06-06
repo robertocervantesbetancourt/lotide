5d3ee2144cda94ae64dd2b457aedfa2199140dd4
Roberto Cervantes Betancourt <roberto.cervantes.bet@gmail.com>
2022-06-06 21:00:40 +0000

    Add module export and separate test code

--- head.js
+++ head.js
@@ -1,18 +1,7 @@
+const assertEqual = require('./assertEqual');
+
 function head(arr){
   return arr[0];
 };
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual}  ===  ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-assertEqual(head([5,6,7]),5);
-assertEqual(head([5,6,7]),6);
-assertEqual(head(['Hello','Bye','Love']),'Hello');
-console.log(head([2,3,4]));
-console.log(head([1]));
-console.log(head([]))
+module.exports = head;
