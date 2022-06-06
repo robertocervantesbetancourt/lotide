51675656fe8a0d382241c1249f902e535dc9ce93
Roberto Cervantes Betancourt <roberto.cervantes.bet@gmail.com>
2022-06-06 21:57:49 +0000

    Export funcion, move test cases to new file

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual}  ===  ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(arr1, arr2) {
   for (let x = 0; x < arr1.length; x++) {
     if (arr1[x] !== arr2[x]) {
@@ -14,7 +6,5 @@ const eqArrays = function(arr1, arr2) {
   } return true;
 };
 
-assertEqual(eqArrays(["1","2","3"], ["1","2","3"]), true);
-assertEqual(eqArrays(["1","2","3"], ["1","2",3]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
+module.exports = eqArrays;
+
