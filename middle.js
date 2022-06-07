d98cbe86138dd975a54da76fac9962659e6f3160
Roberto Cervantes Betancourt <roberto.cervantes.bet@gmail.com>
2022-06-07 17:10:12 +0000

    refactor to return correct value

--- middle.js
+++ middle.js
@@ -2,20 +2,17 @@ const middle = function(arr) {
   let middleArr = [];
   let middleVal = 0;
   if (arr.length === 1 || arr.length === 2) {
-    console.log(middleArr);
-    return;
+    return middleArr;
   }
   if (arr.length % 2 === 0) {
     middleVal = arr.length / 2;
     middleArr.push(arr[middleVal - 1]);
     middleArr.push(arr[middleVal]);
-    console.log(middleArr);
-    return;
+    return middleArr;
   } else {
     middleVal = arr.length / 2;
     middleArr.push(arr[Math.floor(middleVal)]);
-    console.log(middleArr);
-    return;
+    return middleArr;
   }
 };
 
