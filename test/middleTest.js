202ded0f7eb94a3423ac51c25104ad395d5d7b5a
Roberto Cervantes Betancourt <roberto.cervantes.bet@gmail.com>
2022-06-07 19:00:11 +0000

    add chai test to middleTest

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,20 @@
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
-const eqArrays = require('../eqArrays');
+const assert = require('chai').assert;
 
-assertArraysEqual(eqArrays(middle([1]),[]));
-assertArraysEqual(eqArrays(middle([1,2]),[]));
-assertArraysEqual(eqArrays(middle([1,2,3]),[2]));
-assertArraysEqual(eqArrays(middle([1,2,3,4]),[2,3]));
+describe ('#middle', () => {
+  it('should return [] for [1]', () => {
+    assert.deepEqual(middle([1]), []);
+  });
+
+  it('should return [] for [1,2]', () => {
+    assert.deepEqual(middle([1,2]), []);
+  });
+
+  it('should return [2] for [1, 2, 3]', () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it('should return [2, 3] for [1, 2, 3, 4]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2,3]);
+  });
+});
