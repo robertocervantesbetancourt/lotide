9497b096234c78efe51918a6ff40bf430d231836
Roberto Cervantes Betancourt <roberto.cervantes.bet@gmail.com>
2022-06-07 20:28:35 +0000

    eslint

--- flatten.js
+++ flatten.js
@@ -1,20 +1,17 @@
-
 const flatten = function(arr) {
   let flattenArray = [];
-  for (let x = 0; x < arr.length; x++){
+  for (let x = 0; x < arr.length; x++) {
     let results = Array.isArray(arr[x]);
-    //console.log(results);
     if (results === false) {
       flattenArray.push(arr[x]);
-      //console.log(flattenArray);
     } else {
-      for (let i = 0; i < arr[x].length; i++){
+      for (let i = 0; i < arr[x].length; i++) {
         flattenArray.push(arr[x][i]);
       }
     }
   }
   console.log(flattenArray);
-  return
-} 
+  return;
+};
   
-flatten([1, 2, [3, 4], 5, [6]])
+// flatten([1, 2, [3, 4], 5, [6]]);
