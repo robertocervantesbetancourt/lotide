12ec3e1f8ef36790ae5b936dcb07673b25a7967b
Roberto Cervantes Betancourt <roberto.cervantes.bet@gmail.com>
2022-06-07 20:41:21 +0000

    eslint and module export

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,32 +1,34 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual}  ===  ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅✅✅ Assertion Passed: ${actual}  ===  ${expected}`);
+//   } else {
+//     console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
 
 const findKeyByValue = function(obj, val) {
   let key = undefined;
   
-  for (const element in obj){
-    if(obj[element] === val){
+  for (const element in obj) {
+    if (obj[element] === val) {
       key = element;
-    } 
+    }
   }
   console.log(key);
   return key;
-}
-
-const bestTVShowsByGenre = {
-  sci_fi: 'The Expanse',
-  comedy: 'Brooklyn Nine-Nine',
-  drama: 'The Wire'
 };
 
-assertEqual(findKeyByValue(bestTVShowsByGenre, 'That 70s Show'),undefined);
-assertEqual(findKeyByValue(bestTVShowsByGenre, 'The Expanse'),'sci_fi');
-assertEqual(findKeyByValue(bestTVShowsByGenre, 'The Wire'),'drama');
-assertEqual(findKeyByValue(bestTVShowsByGenre, 'Brooklyn Nine-Nine'),'comedy');
+module.exports = findKeyByValue;
+
+// const bestTVShowsByGenre = {
+//   sci_fi: 'The Expanse',
+//   comedy: 'Brooklyn Nine-Nine',
+//   drama: 'The Wire'
+// };
+
+// assertEqual(findKeyByValue(bestTVShowsByGenre, 'That 70s Show'),undefined);
+// assertEqual(findKeyByValue(bestTVShowsByGenre, 'The Expanse'),'sci_fi');
+// assertEqual(findKeyByValue(bestTVShowsByGenre, 'The Wire'),'drama');
+// assertEqual(findKeyByValue(bestTVShowsByGenre, 'Brooklyn Nine-Nine'),'comedy');
 
 //findKeyByValue(bestTVShowsByGenre, 'Brooklyn Nine-Nine')
