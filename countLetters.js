01f1eee5314f95deabb610b394530c50707b5cf5
Roberto Cervantes Betancourt <roberto.cervantes.bet@gmail.com>
2022-06-07 20:38:09 +0000

    eslint and module export

--- countLetters.js
+++ countLetters.js
@@ -1,28 +1,30 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual}  ===  ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅✅✅ Assertion Passed: ${actual}  ===  ${expected}`);
+//   } else {
+//     console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
 
 const countLetters = function(sentence) {
   const result = {};
   for (const letter of sentence) {
-    if (letter !== ' '){
-      if (result[letter]){
+    if (letter !== ' ') {
+      if (result[letter]) {
         result[letter] += 1;
       } else {
         result[letter] = 1;
       }
-    } 
+    }
   }
   console.log(result);
   return result;
 };
 
-const result1 = countLetters('lighthouse in the house');
+module.exports = countLetters;
 
-assertEqual(result1['h'],4);
-assertEqual(result1['e'],3);
-assertEqual(result1['z'],undefined);
+// const result1 = countLetters('lighthouse in the house');
+
+// assertEqual(result1['h'],4);
+// assertEqual(result1['e'],3);
+// assertEqual(result1['z'],undefined);
