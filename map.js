6bfeabb8b1dd78706f003e9d8bebc680f4079be9
Roberto Cervantes Betancourt <roberto.cervantes.bet@gmail.com>
2022-06-08 15:57:42 +0000

    eslint and module export

--- map.js
+++ map.js
@@ -1,35 +1,41 @@
-const eqArrays = function(arr1, arr2) {
-  for (let x = 0; x < arr1.length; x++) {
-    if (arr1[x] !== arr2[x]) {
-      return false;
-    }
-  } 
-  return true;
-};
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
-const assertArraysEqual = function(value) {
-  if (value === false) {
-    console.log('Arrays are NOT equal');
-    return
-  } 
-  console.log('Both arrays are equal');
-  return
-}
-
-
-const words = ["ground", "control", "to", "major", "tom"];
-
-
-const map = function(array, callback){
+const map = function(array, callback) {
   const results = [];
-  for (let item of array){
+  for (let item of array) {
     results.push(callback(item));
   }
-
   return results;
-}
+};
 
-assertArraysEqual(eqArrays(map(words, word => word),words));
+module.exports = map;
 
-const result = map(words, word => word[0]);
-console.log(result);
+
+// const eqArrays = function(arr1, arr2) {
+//   for (let x = 0; x < arr1.length; x++) {
+//     if (arr1[x] !== arr2[x]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
+
+// const assertArraysEqual = function(value) {
+//   if (value === false) {
+//     console.log('Arrays are NOT equal');
+//     return
+//   }
+//   console.log('Both arrays are equal');
+//   return
+// }
+
+
+// const words = ["ground", "control", "to", "major", "tom"];
+
+
+
+// assertArraysEqual(eqArrays(map(words, word => word),words));
+
+// const result = map(words, word => word[0]);
+// console.log(result);
