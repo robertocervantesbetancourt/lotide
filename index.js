46e007e59277d60ddba4e8154613c5b2e589e482
Roberto Cervantes Betancourt <roberto.cervantes.bet@gmail.com>
2022-06-08 22:25:40 +0000

    object short hand refactor

---
+++ index.js
@@ -0,0 +1,40 @@
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
+const without = require('./without');
+const flatten = require('./flatten');
+const countOnly = require('./countOnly');
+const countLetters = require('./countLetters');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+const eqObject = require('./eqObject');
+const assertObjectsEquals = require('./assertObjectsEqual');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const findKey = require('./findKey');
+
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  assertEqual,
+  eqArrays,
+  assertArraysEqual,
+  without,
+  flatten,
+  countOnly,
+  countLetters,
+  findKeyByValue,
+  letterPositions,
+  eqObject,
+  assertObjectsEquals,
+  map,
+  takeUntil,
+  findKey,
+
+};
+
