f4f4a7c8658cd891e1ffc9912c1ab834a618e8c4
Roberto Cervantes Betancourt <roberto.cervantes.bet@gmail.com>
2022-06-10 18:13:05 +0000

    Remove commented code, added explainer comment

--- countOnly.js
+++ countOnly.js
@@ -1,3 +1,5 @@
+//count how many of the items from the second object are present in the first object
+
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
   for (const item of allItems) {
@@ -9,28 +11,8 @@ const countOnly = function(allItems, itemsToCount) {
       }
     }
   }
-  //console.log(results);
-
   return results;
 };
 
 module.exports = countOnly;
 
-// const firstNames = [
-//   'Karl',
-//   'Salima',
-//   'Agouhanna',
-//   'Fang',
-//   'Kavith',
-//   'Jason',
-//   'Salima',
-//   'Fang',
-//   'Joe'
-// ];
-
-// const result1 = countOnly(firstNames, {'Jason': true, 'Karima': true, 'Fang': true, 'Agouhanna': false});
-
-// assertEqual(result1['Jason'],1);
-// assertEqual(result1['Karima'], undefined);
-// assertEqual(result1['Fang'],2);
-// assertEqual(result1['Agouhanna'], undefined);
