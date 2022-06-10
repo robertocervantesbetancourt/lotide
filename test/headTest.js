8c9b212febdf7821dceecf4111c380f7bd55a433
Roberto Cervantes Betancourt <roberto.cervantes.bet@gmail.com>
2022-06-10 18:38:58 +0000

    refactor to moche & chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,7 @@
 const head = require('../head');
 const assert = require('chai').assert;
 
-describe('#head',()=>{
+describe('#head',() => {
   it('returns 1 for [1, 2, 3]', () => {
     assert.strictEqual(head([1, 2, 3]),1)
   });
