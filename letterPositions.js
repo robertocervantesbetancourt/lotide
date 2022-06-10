0b18437e24a448f1671a9a36f5ebd79edaf17178
Roberto Cervantes Betancourt <roberto.cervantes.bet@gmail.com>
2022-06-10 17:03:38 +0000

    Remove unnecessary code

--- letterPositions.js
+++ letterPositions.js
@@ -11,7 +11,7 @@ const letterPositions = function(sentence) {
       index++;
     }
   }
-  console.log(result);
+  //console.log(result);
   return result;
 };
 
@@ -19,28 +19,3 @@ module.exports = letterPositions;
 
 
 
-// const eqArrays = function(arr1, arr2) {
-//   for (let x = 0; x < arr1.length; x++) {
-//     if (arr1[x] !== arr2[x]) {
-//       return false;
-//     }
-//   }
-//   return true;
-// };
-
-// const assertArraysEqual = function(value) {
-//   if (value === false) {
-//     console.log('Arrays are NOT equal');
-//     return
-//   }
-//   console.log('Both arrays are equal');
-//   return
-// }
-
-
-// const result1 = letterPositions('hello');
-
-// assertArraysEqual(eqArrays(result1['h'],[0]));
-// assertArraysEqual(eqArrays(result1['e'],[1]));
-// assertArraysEqual(eqArrays(result1['l'],[2,3]));
-// assertArraysEqual(eqArrays(result1['o'],[4]));
