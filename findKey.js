d8cc888a6af1ef4583a73b4397b9cd7411ddec14
Roberto Cervantes Betancourt <roberto.cervantes.bet@gmail.com>
2022-06-10 18:36:56 +0000

    remove commented code, add explainer comment

--- findKey.js
+++ findKey.js
@@ -1,3 +1,5 @@
+//find the key that matches the callback condition
+
 const findKey = function(object, callback) {
   for (let key in object) {
     if (callback(object[key])) {
@@ -9,21 +11,3 @@ const findKey = function(object, callback) {
 
 module.exports = findKey;
 
-
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`✅✅✅ Assertion Passed: ${actual}  ===  ${expected}`);
-//   } else {
-//     console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-//   }
-// };
-
-
-// console.log(findKey({
-//   "Blue Hill": { stars: 1 },
-//   "Akaleri":   { stars: 3 },
-//   "noma":      { stars: 2 },
-//   "elBulli":   { stars: 3 },
-//   "Ora":       { stars: 2 },
-//   "Akelarre":  { stars: 3 }
-// }, x => x.stars === 2))
