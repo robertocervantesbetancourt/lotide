355b311dd29fecfd8f3b851fdbd558f683731def
Roberto Cervantes Betancourt <roberto.cervantes.bet@gmail.com>
2022-06-10 18:33:53 +0000

    remove commented code

--- map.js
+++ map.js
@@ -1,5 +1,4 @@
-const eqArrays = require('./eqArrays');
-const assertArraysEqual = require('./assertArraysEqual');
+//return values from array that meet callback condition
 
 const map = function(array, callback) {
   const results = [];
@@ -10,32 +9,3 @@ const map = function(array, callback) {
 };
 
 module.exports = map;
-
-
-// const eqArrays = function(arr1, arr2) {
-//   for (let x = 0; x < arr1.length; x++) {
-//     if (arr1[x] !== arr2[x]) {
-//       return false;
-//     }
-//   }
-//   return true;
-// };
-
-// const assertArraysEqual = function(value) {
-//   if (value === false) {
-//     console.log('Arrays are NOT equal');
-//     return
-//   }
-//   console.log('Both arrays are equal');
-//   return
-// }
-
-
-// const words = ["ground", "control", "to", "major", "tom"];
-
-
-
-// assertArraysEqual(eqArrays(map(words, word => word),words));
-
-// const result = map(words, word => word[0]);
-// console.log(result);
