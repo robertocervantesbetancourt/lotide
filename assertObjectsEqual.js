5205b5bc8091aa13f64ebd588d7546eb171cb0ed
Roberto Cervantes Betancourt <roberto.cervantes.bet@gmail.com>
2022-06-10 18:31:40 +0000

    remove commented code, include explainer comment

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,5 +1,4 @@
-const eqArrays = require('./eqArrays');
-const eqObject = require('./eqObject');
+//Assertion to check eqObject function
 
 const assertObjectsEquals = function(actual, expected) {
   if (actual === expected) {
@@ -12,44 +11,3 @@ const assertObjectsEquals = function(actual, expected) {
 };
 
 module.exports = assertObjectsEquals;
-
-// const eqArrays = function(arr1, arr2) {
-//   if (arr1.length !== arr2.length) {
-//     return false;
-//   } else {
-//     for (let x = 0; x < arr1.length; x++) {
-//       if (arr1[x] !== arr2[x]) {
-//         return false;
-//       }
-//     }
-//   }
-//   return true;
-// };
-
-// const eqObjects = function(object1, object2) {
-//   if (Object.keys(object1).length === Object.keys(object2).length) {
-//     for (const elem in object1) {
-//       if (Array.isArray(object1[elem])) {
-//         if (!(eqArrays(object1[elem], object2[elem]))) {
-//           return false;
-//         }
-//       } else {
-//         if (object1[elem] !== object2[elem]) {
-//           return false;
-//         }
-//       }
-//     }
-//     return true;
-//   }
-//   return false;
-// };
-
-
-// const ab = {a:'1', b:'2'};
-// const ba = {b:'2', a:'1'};
-// const abc = {a:'1', b:'2', c:'3'};
-// const cd = { c: "1", d: ["2", 3] };
-// const dc = { d: ["2", 3], c: "1" };
-// const cd2 = { c: "1", d: ["2", 3, 4] };
-
-// assertObjectsEquals(eqObjects(ab,ba),true);
