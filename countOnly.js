31631ffbf0e6411c658b515fb157edd344ccdb40
Roberto Cervantes Betancourt <roberto.cervantes.bet@gmail.com>
2022-06-10 16:49:22 +0000

    clean file and removed unnecessary code

--- countOnly.js
+++ countOnly.js
@@ -1,11 +1,3 @@
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`✅✅✅ Assertion Passed: ${actual}  ===  ${expected}`);
-//   } else {
-//     console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-//   }
-// };
-
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
   for (const item of allItems) {
@@ -17,7 +9,7 @@ const countOnly = function(allItems, itemsToCount) {
       }
     }
   }
-  console.log(results);
+  //console.log(results);
 
   return results;
 };
