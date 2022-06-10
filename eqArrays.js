7a60d67b4b0ed76838b8930e8b0d8f73bed3a5be
Roberto Cervantes Betancourt <roberto.cervantes.bet@gmail.com>
2022-06-10 18:06:12 +0000

    Include explainer comment

--- eqArrays.js
+++ eqArrays.js
@@ -1,4 +1,9 @@
+//compare two arrays an return tru if they are the same, false if they are different
+
 const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length){
+    return false;
+  }
   for (let x = 0; x < arr1.length; x++) {
     if (arr1[x] !== arr2[x]) {
       return false;
