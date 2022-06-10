0bae74049a8af3178ad3e76b08be5aefec65054e
Roberto Cervantes Betancourt <roberto.cervantes.bet@gmail.com>
2022-06-10 17:07:04 +0000

    Removed unnecessary code

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,11 +1,3 @@
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`✅✅✅ Assertion Passed: ${actual}  ===  ${expected}`);
-//   } else {
-//     console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-//   }
-// };
-
 const findKeyByValue = function(obj, val) {
   let key = undefined;
   
@@ -14,21 +6,8 @@ const findKeyByValue = function(obj, val) {
       key = element;
     }
   }
-  console.log(key);
+  //console.log(key);
   return key;
 };
 
 module.exports = findKeyByValue;
-
-// const bestTVShowsByGenre = {
-//   sci_fi: 'The Expanse',
-//   comedy: 'Brooklyn Nine-Nine',
-//   drama: 'The Wire'
-// };
-
-// assertEqual(findKeyByValue(bestTVShowsByGenre, 'That 70s Show'),undefined);
-// assertEqual(findKeyByValue(bestTVShowsByGenre, 'The Expanse'),'sci_fi');
-// assertEqual(findKeyByValue(bestTVShowsByGenre, 'The Wire'),'drama');
-// assertEqual(findKeyByValue(bestTVShowsByGenre, 'Brooklyn Nine-Nine'),'comedy');
-
-//findKeyByValue(bestTVShowsByGenre, 'Brooklyn Nine-Nine')
